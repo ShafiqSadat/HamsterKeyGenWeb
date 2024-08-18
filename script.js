5e994005ee05dbf971572d05157dea8522145d82
Shafiq Sadat <ShafiqSadat@users.noreply.github.com>
2024-08-18 16:12:06 +0430

    Added individual timing and attempt settings for each game in promo code generator script. Implemented configurable delays and retry logic per game to enhance flexibility and control over the key generation process.

--- script.js
+++ script.js
@@ -1,36 +1,46 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const EVENTS_DELAY = 20000;
-
     const games = {
         1: {
             name: 'Riding Extreme 3D',
             appToken: 'd28721be-fd2d-4b45-869e-9f253b554e50',
             promoId: '43e35910-c168-4634-ad4f-52fd764a843f',
+            timing: 30000 / 1000, // 30 seconds
+            attempts: 25,
         },
         2: {
             name: 'Chain Cube 2048',
             appToken: 'd1690a07-3780-4068-810f-9b5bbf2931b2',
             promoId: 'b4170868-cef0-424f-8eb9-be0622e8e8e3',
+            timing: 30000 / 1000, // 30 seconds
+            attempts: 20,
         },
         3: {
             name: 'My Clone Army',
             appToken: '74ee0b5b-775e-4bee-974f-63e7f4d5bacb',
             promoId: 'fe693b26-b342-4159-8808-15e3ff7f8767',
+            timing: 180000 / 1000, // 180 seconds
+            attempts: 30,
         },
         4: {
             name: 'Train Miner',
             appToken: '82647f43-3f87-402d-88dd-09a90025313f',
             promoId: 'c4480ac7-e178-4973-8061-9ed5b2e17954',
+            timing: 30000 / 1000, // 30 seconds
+            attempts: 15,
         },
         5: {
             name: 'Merge Away',
             appToken: '8d1cc2ad-e097-4b86-90ef-7a27e19fb833',
-            promoId: 'dc128d28-c45b-411c-98ff-ac7726fbaea4'
+            promoId: 'dc128d28-c45b-411c-98ff-ac7726fbaea4',
+            timing: 30000 / 1000, // 30 seconds
+            attempts: 25,
         },
         6: {
             name: 'Twerk Race 3D',
             appToken: '61308365-9d16-4040-8bb0-2f4a4c69074c',
-            promoId: '61308365-9d16-4040-8bb0-2f4a4c69074c'
+            promoId: '61308365-9d16-4040-8bb0-2f4a4c69074c',
+            timing: 30000 / 1000, // 30 seconds
+            attempts: 20,
         }
     };
 
@@ -115,8 +125,8 @@ document.addEventListener('DOMContentLoaded', () => {
                 return null;
             }
 
-            for (let i = 0; i < 25; i++) {
-                await sleep(EVENTS_DELAY * delayRandom());
+            for (let i = 0; i < game.attempts; i++) {
+                await sleep(game.timing * delayRandom());
                 const hasCode = await emulateProgress(clientToken, game.promoId);
                 updateProgress(7 / keyCount, 'Emulating progress...');
                 if (hasCode) {
