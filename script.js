eb80316ff9b09a6169a53ca80583471dad4610fb
Shafiq Sadat <ShafiqSadat@users.noreply.github.com>
2024-08-30 14:28:12 +0430

    Update script.js

--- script.js
+++ script.js
@@ -81,8 +81,8 @@ document.addEventListener('DOMContentLoaded', () => {
             name: 'Gangs Wars',
             appToken: 'b6de60a0-e030-48bb-a551-548372493523',
             promoId: 'c7821fa7-6632-482c-9635-2bd5798585f9',
-            timing: 30000, // 20 seconds
-            attempts: 20,
+            timing: 40000, // 40 seconds
+            attempts: 30,
         }
     };
 
