78471df1dad0dd66a9b2774dbf9694d833e01539
Shafiq Sadat <ShafiqSadat@users.noreply.github.com>
2024-09-04 12:35:39 +0430

    Added support Tile Trio and Fluff Crusade!

--- script.js
+++ script.js
@@ -64,9 +64,9 @@ document.addEventListener('DOMContentLoaded', () => {
             attempts: 20,
         },
         10: {
-            name: 'Cafe Dash',
-            appToken: 'bc0971b8-04df-4e72-8a3e-ec4dc663cd11',
-            promoId: 'bc0971b8-04df-4e72-8a3e-ec4dc663cd11',
+            name: 'Tile Trio',
+            appToken: 'e68b39d2-4880-4a31-b3aa-0393e7df10c7',
+            promoId: 'e68b39d2-4880-4a31-b3aa-0393e7df10c7',
             timing: 20000, // 20 seconds
             attempts: 20,
         },
@@ -78,10 +78,10 @@ document.addEventListener('DOMContentLoaded', () => {
             attempts: 20,
         },
         12: {
-            name: 'Gangs Wars',
-            appToken: 'b6de60a0-e030-48bb-a551-548372493523',
-            promoId: 'c7821fa7-6632-482c-9635-2bd5798585f9',
-            timing: 40000, // 40 seconds
+            name: 'Fluff Crusade',
+            appToken: '112887b0-a8af-4eb2-ac63-d82df78283d9',
+            promoId: '112887b0-a8af-4eb2-ac63-d82df78283d9',
+            timing: 20000, // 40 seconds
             attempts: 30,
         }
     };
