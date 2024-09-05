5c6d382beb4d098c3e505dcf8062525690024577
Shafiq Sadat <ShafiqSadat@users.noreply.github.com>
2024-09-05 20:42:35 +0430

    Added support for Stone Age!

--- script.js
+++ script.js
@@ -126,6 +126,13 @@ document.addEventListener('DOMContentLoaded', () => {
             promoId: '112887b0-a8af-4eb2-ac63-d82df78283d9',
             timing: 20000, // 40 seconds
             attempts: 30,
+        },
+        13: {
+            name: 'Stone Age',
+            appToken: '04ebd6de-69b7-43d1-9c4b-04a6ca3305af',
+            promoId: '04ebd6de-69b7-43d1-9c4b-04a6ca3305af',
+            timing: 20000, // 40 seconds
+            attempts: 30,
         }
     };
 
