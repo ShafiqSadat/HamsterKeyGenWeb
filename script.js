3d5f9159b5f05183a3e0d10896c8bc659ddeb393
Shafiq Sadat <ShafiqSadat@users.noreply.github.com>
2024-09-19 15:58:33 +0430

    Added support for Factory World!

--- script.js
+++ script.js
@@ -175,6 +175,13 @@ document.addEventListener('DOMContentLoaded', () => {
             promoId: 'daab8f83-8ea2-4ad0-8dd5-d33363129640',
             timing: 20000, // 30 seconds
             attempts: 30,
+        },
+        20: {
+            name: 'Factory World',
+            appToken: 'd02fc404-8985-4305-87d8-32bd4e66bb16',
+            promoId: 'd02fc404-8985-4305-87d8-32bd4e66bb16',
+            timing: 20000, // 30 seconds
+            attempts: 30,
         }
     };
 
